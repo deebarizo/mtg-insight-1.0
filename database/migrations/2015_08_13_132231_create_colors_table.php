bdcab07d4f76ebb19339910cd46fc735dd21a014
Dee Barizo <deebarizo@gmail.com>
2015-08-13 18:13:03 +0000

    Add migrations

---
+++ database/migrations/2015_08_13_132231_create_colors_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateColorsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('colors', function($table)
+        {
+            $table->increments('id');
+            $table->string('color')->unique();
+            $table->date('created_at');
+            $table->date('updated_at');
+        });   
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('colors');
+    }
+}
