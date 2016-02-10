81d289c95a2158b20e0280565150778cb21a0da5
Dee Barizo <deebarizo@gmail.com>
2016-02-10 23:43:32 +0000

    Download all the CDN css and js so I can use Freedom App

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -5,17 +5,17 @@
 	<head>
 		<meta charset="UTF-8">
 
-		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
-		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap-theme.min.css">
+		<link rel="stylesheet" href="/css/bootstrap.min.css">
+		<link rel="stylesheet" href="/css/bootstrap-theme.min.css">
 		<link rel="stylesheet" href="/css/mtg-font-master/css/magic-font.css" /> <!-- Mana Symbols -->
-		<link rel="stylesheet" href="//cdn.datatables.net/1.10.8/css/jquery.dataTables.min.css">
+		<link rel="stylesheet" href="/css/jquery.dataTables.min.css">
 		<link rel="stylesheet" href="/css/jquery.qtip.min.css">
 		<link rel="stylesheet" href="/css/style.css">
 
-		<script src="//code.jquery.com/jquery-1.11.3.min.js"></script>
-		<script src="//code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
-		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
-		<script src="//cdn.datatables.net/1.10.8/js/jquery.dataTables.min.js"></script>
+		<script src="/js/jquery-1.11.3.min.js"></script>
+		<script src="/js/jquery-migrate-1.2.1.min.js"></script>
+		<script src="/js/bootstrap.min.js"></script>
+		<script src="/js/jquery.dataTables.min.js"></script>
 		<script src="/js/jquery.qtip.min.js"></script>
 		<script src="/js/highcharts.js"></script>
 
